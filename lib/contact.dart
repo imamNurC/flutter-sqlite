bf41fa5fdd214f38bb1fac7b7059f798eb8b9d5b
Mr.Ackerman <imamnurcakra@gmail.com>
2024-06-13 11:55:30 +0700

    conact

---
+++ lib/contact.dart
@@ -0,0 +1,25 @@
+class Contact {
+  int id;
+  String name;
+  String email;
+  String phone;
+
+  Contact({
+      required this.id,
+      required this.name,
+      required this.email,
+      required this.phone
+  });
+
+  Map<String, dynamic> toMap() =>
+      {"id": id, "name": name, "email": email, "phone": phone};
+
+  factory Contact.fromMap(Map<String, dynamic> map) => Contact(
+        id: map["id"],
+        name: map["name"],
+        email: map['email'],
+        phone: map["phone"],
+      );
+
+  // static fromMap(Map<String, Object?> e) {}
+}
